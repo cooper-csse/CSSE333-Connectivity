ba7096bcc357770fed0f2dca1882d7945a769317
Amanda Stouder <stouder@rose-hulman.edu>
2018-03-30 09:26:11 -0400

    Adding notes to prevent students from changing the given URL

--- src/sodabase/services/DatabaseConnectionService.java
+++ src/sodabase/services/DatabaseConnectionService.java
@@ -4,7 +4,8 @@ import java.sql.Connection;
 
 public class DatabaseConnectionService {
 
-	private String url = "jdbc:sqlserver://${dbServer};databaseName=${dbName};user=${user};password={${pass}}";
+	//DO NOT EDIT THIS STRING, YOU WILL RECEIVE NO CREDIT FOR THIS TASK IF THIS STRING IS EDITED
+	private final String SampleURL = "jdbc:sqlserver://${dbServer};databaseName=${dbName};user=${user};password={${pass}}";
 
 	private Connection connection = null;
 
@@ -12,12 +13,14 @@ public class DatabaseConnectionService {
 	private String serverName;
 
 	public DatabaseConnectionService(String serverName, String databaseName) {
+		//DO NOT CHANGE THIS METHOD
 		this.serverName = serverName;
 		this.databaseName = databaseName;
 	}
 
 	public boolean connect(String user, String pass) {
 		//TODO: Task 1
+		//BUILD YOUR CONNECTION STRING HERE USING THE SAMPLE URL ABOVE
 		return false;
 	}
 	
